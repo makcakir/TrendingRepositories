d0b6833213a10146f1a3a7c05ada7916a2667f62
Mustafa Ali Akçakır <mustafaaliakcakir@gmail.com>
2023-01-11 20:07:19 +0300

    Add TrendingRepositoryPresentation.

---
+++ TrendingRepositories/Features/TrendingRepositories/TrendingRepositoryPresentation.swift
@@ -0,0 +1,17 @@
+//
+//  TrendingRepositoryPresentation.swift
+//  TrendingRepositories
+//
+//  Created by Mustafa Ali Akçakır on 11.01.2023.
+//
+
+import Foundation
+
+struct TrendingRepositoryPresentation {
+    let ownerImageUrl: String
+    let ownerName: String
+    let title: String
+    let description: String
+    let language: String
+    let starCount: Int
+}
