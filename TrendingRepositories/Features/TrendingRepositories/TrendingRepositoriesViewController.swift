7979e8f99c4b6610fe80de9cddeb8fa68a41762b
Mustafa Ali Akçakır <mustafaaliakcakir@gmail.com>
2023-01-11 20:02:44 +0300

    Add TrendingRepositoriesViewController and place it inside navigation controller.

---
+++ TrendingRepositories/Features/TrendingRepositories/TrendingRepositoriesViewController.swift
@@ -0,0 +1,24 @@
+//
+//  TrendingRepositoriesViewController.swift
+//  TrendingRepositories
+//
+//  Created by Mustafa Ali Akçakır on 11.01.2023.
+//
+
+import UIKit
+
+class TrendingRepositoriesViewController: UITableViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setupSubviews()
+    }
+}
+
+private extension TrendingRepositoriesViewController {
+    
+    func setupSubviews() {
+        title = "Trending"
+    }
+}
--- TrendingRepositories/SceneDelegate.swift
+++ TrendingRepositories/SceneDelegate.swift
@@ -18,10 +18,12 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = (scene as? UIWindowScene) else {
             return
         }
-        
+        let viewController = TrendingRepositoriesViewController()
+        let navigationController = UINavigationController(rootViewController: viewController)
+
         window = UIWindow(frame: windowScene.coordinateSpace.bounds)
         window?.windowScene = windowScene
-        window?.rootViewController = ViewController()
+        window?.rootViewController = navigationController
         window?.makeKeyAndVisible()
     }
     
